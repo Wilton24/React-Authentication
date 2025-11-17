81b669c8bcd5077aabc7f4a0cdc7ecb2d57d68e6
Wilton Batiquin <cbasher24@gmail.com>
2025-11-17 16:37:43 +0800

    added logout function

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,9 +1,10 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Form } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
+
   return (
     <header className={classes.header}>
       <nav>
@@ -48,6 +49,11 @@ function MainNavigation() {
             >
               Authentication
             </NavLink>
+          </li>
+          <li>
+            <Form method="post" action="/logout">
+              <button type="submit">Logout</button>
+            </Form>
           </li>
         </ul>
       </nav>
