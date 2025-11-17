44510b548e214683c1b43d5e68e4e82ad313e345
Wilton Batiquin <cbasher24@gmail.com>
2025-11-17 20:31:31 +0800

    Implement token expiration handling and add authentication check loaders

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -35,22 +35,6 @@ export async function action({ request, params }) {
     body: JSON.stringify(authData),
   });
 
-  // if (!response.ok) {
-  //   let errorResData;
-  //   let errorMessage = 'Authentication failed!';
-
-  //   try {
-  //     errorResData = await response.json();
-  //   } catch (err) {
-  //   }
-
-  //   if (errorResData && errorResData.error && errorResData.error.message) {
-  //     errorMessage = errorResData.error.message;
-  //   }
-
-  //   console.log("HERE is the cause:", errorMessage);
-  //   throw new Error(errorMessage);
-  // }
 
   if (response.status === 422 || response.status === 401) {
     return response;
@@ -64,10 +48,13 @@ export async function action({ request, params }) {
   // const expirationTime = new Date(
   //   new Date().getTime() + +resData.expiresIn * 1000
   // );
+  const expirationTime = new Date();
+  expirationTime.setHours(expirationTime.getHours() + 1); // 1 hour expiration
+
+  // Store token and expiration time in local storage
 
   localStorage.setItem('token', resData.token);
-  // localStorage.setItem('expirationTime', expirationTime.toISOString());
-
+  localStorage.setItem('expirationTime', expirationTime.toISOString());
 
   return redirect('/');
 }
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,10 +1,34 @@
 
 
+export function getTokenDuration() {
+    const expirationDate = localStorage.getItem('expiration');
+    const now = new Date();
+    const expiration = new Date(expirationDate);
+    const duration = expiration.getTime() - now.getTime();
+    return duration;
+}
+
 export function getAuthToken() {
     const token = localStorage.getItem('token');
+
+    const tokenDuration = getTokenDuration(); // Optional: you can use this to check token validity
+    if (tokenDuration <= 0) {
+        // localStorage.removeItem('token');
+        // localStorage.removeItem('expiration');
+        // return null;
+        return "EXPIRED";
+    };
     return token;
 }
 
 export function tokenLoader() {
     return getAuthToken();
 }
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+    if (!token) {
+        throw new Response('Unauthorized', { status: 401 });
+    }
+    return token;
+};
